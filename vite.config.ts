ece0dd6eac2dea02c832d82a20e51748b5e2f151
lsvishaal <lsvishaal7@gmail.com>
2024-08-26 11:42:40 +0530

    Installed vite-plugin-checker, Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({ typescript: false }) // Disable TypeScript type checking
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
